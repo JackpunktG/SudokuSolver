2322fe3fc44dcbdb1f2e3503ed2db5a3ed3067ce
JackpunktG <Jack.goldsbrough@outlook.com>
2025-08-14 18:12:47 +0200

    D

--- obj/Debug/net8.0/SudokuSolver.AssemblyInfo.cs
+++ obj/Debug/net8.0/SudokuSolver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SudokuSolver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7912861bf2540883104dd7b8c07798aa716500ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("SudokuSolver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SudokuSolver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
