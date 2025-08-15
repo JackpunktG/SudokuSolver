d3a6ba57427fa51ce579e880b8e726e81f618b00
JackpunktG <Jack.goldsbrough@outlook.com>
2025-08-15 21:25:19 +0200

    finished the draft of the pair guessing logic

--- Program.cs
+++ Program.cs
@@ -1,30 +1,40 @@
 ﻿
 
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using Sudoku;
 
-int[] puzzle1 = new int[81]
+string fileName = args[0];
+
+string[] lines = File.ReadAllLines(fileName);
+
+foreach (var line in lines)
 {
-    0, 0, 0, 2, 6, 0, 7, 0, 1,
-    6, 8, 0, 0, 7, 0, 0, 9, 0,
-    1, 9, 0, 0, 0, 4, 5, 0, 0,
-    8, 2, 0, 1, 0, 0, 0, 4, 0,
-    0, 0, 4, 6, 0, 2, 9, 0, 0,
-    0, 5, 0, 0, 0, 3, 0, 2, 8,
-    0, 0, 9, 3, 0, 0, 0, 7, 4,
-    0, 4, 0, 0, 5, 0, 0, 3, 6,
-    7, 0, 3, 0, 1, 8, 0, 0, 0
-};
+    string[] p = line.Split(',');
+    string name = p[0];
+    int[] puzzle = p[1..].Select(s => int.Parse(s)).ToArray();
 
-var puzzle = new SudokuPuzzle(puzzle1);
+    var sudoku = new SudokuPuzzle(name, puzzle);
 
+    Stopwatch sw = Stopwatch.StartNew();
+    sudoku.Solve();
+    sw.Stop();
+    sudoku.PrintSudoku();
+    Console.WriteLine($"Execution Time: {sw.Elapsed.TotalSeconds} seconds\n");
+}
 
-Stopwatch sw = Stopwatch.StartNew();
-puzzle.Solve();
-sw.Stop();
-puzzle.PrintSudoku();
+/*
+string test = "Sudoku Medium 3,0,0,0,9,0,0,0,1,5,0,0,1,0,0,0,7,2,9,0,0,0,5,0,0,0,0,0,4,0,0,0,2,0,0,0,0,0,0,8,7,0,9,0,0,0,0,3,0,0,0,0,4,9,7,0,0,3,1,0,6,8,7,0,8,0,0,2,0,3,5,0,1,5,1,6,0,8,0,0,3,0";
 
+string[] p = test.Split(',');
+string name = p[0];
+int[] puzzle = p[1..].Select(s => int.Parse(s)).ToArray();
 
-Console.WriteLine($"Execution Time: {sw.Elapsed.TotalSeconds} seconds");
+ var sudoku = new SudokuPuzzle(name, puzzle);
 
-
+    Stopwatch sw = Stopwatch.StartNew();
+    sudoku.Solve();
+    sw.Stop();
+    sudoku.PrintSudoku();
+    Console.WriteLine($"Execution Time: {sw.Elapsed.TotalSeconds} seconds\n");
+*/
--- Sudoku.cs
+++ Sudoku.cs
@@ -1,29 +1,71 @@
 using System.Diagnostics.Contracts;
+using System.Drawing;
 using System.Dynamic;
+using System.Runtime.CompilerServices;
 
 namespace Sudoku;
 
 public class SudokuPuzzle
 {
+    string name;
     public int[] Sudoku { get; set; }
     int?[,] sudokuPossibilities = new int?[81, 9];
     bool sudokuSolved = false;
 
-    public SudokuPuzzle(int[] puzzle)
+    public SudokuPuzzle(string name, int[] puzzle)
     {
+        this.name = name;
         Sudoku = puzzle;
         SudokuSetter();
     }
 
     public void Solve()
     {
+        int counter = 0;
+        int noChange = 0;
+        Console.WriteLine("\n" + name + "Solving...\n");
 
-        while (sudokuSolved == false)
+        while (!sudokuSolved)
         {
+            counter++;
+            int?[,] startchecker = new int?[81, 9];
+            for (int i = 0; i < 81; i++)
+                for (int k = 0; k < 9; k++)
+                    startchecker[i, k] = sudokuPossibilities[i, k];
+
             RemovePossibilitesCheck();
             SquareChecker();
+            PossibilityCheck();
             IsSolved();
+            bool change = StateChecker(startchecker);
+
+            if (!change)
+            {
+                CheckingPair();
+                noChange++;
+            }
+            
+
+            if (noChange >= 5)
+            {
+                Console.WriteLine("Could not find Solution... :(");
+                return;
+            }
+
         }
+        Console.WriteLine($"Found solution in {counter} loops");
+    }
+
+    public bool StateChecker(int?[,] start)
+    {
+        for (int i = 0; i < 81; i++)
+        {
+            for (int k = 0; k < 9; k++)
+            {
+                if (start[i, k] != sudokuPossibilities[i, k]) return true;
+            }
+        }
+        return false;
     }
 
     public void SudokuSetter()
@@ -45,6 +87,155 @@ public class SudokuPuzzle
         }
     }
 
+    public void CheckingPair()
+    {
+        for (int i = 0; i < 9; i++) RowPair(i);
+        for (int i = 0; i < 9; i++) ColumnPair(i);
+    }
+
+    public void SolveFromPair()
+    {
+        int noChange = 0;
+
+        while (!sudokuSolved)
+        {
+            int?[,] startchecker = new int?[81, 9];
+            for (int i = 0; i < 81; i++)
+                for (int k = 0; k < 9; k++)
+                    startchecker[i, k] = sudokuPossibilities[i, k];
+
+            RemovePossibilitesCheck();
+            SquareChecker();
+            PossibilityCheck();
+            IsSolved();
+            bool change = StateChecker(startchecker);
+
+            if (!change)
+            {
+                CheckingPair();
+                noChange++;
+            }
+            if (noChange > 5) return;
+            
+
+        }
+        
+    }
+
+
+    public void GuessPair(int square, int option1, int option2)
+    {
+        int?[,] possibilitiesState = new int?[81, 9];       //saving the current states
+        for (int i = 0; i < 81; i++)
+            for (int k = 0; k < 9; k++)
+                possibilitiesState[i, k] = sudokuPossibilities[i, k];
+
+        int[] sudokuState = new int[81];
+        for (int i = 0; i < 81; i++) sudokuState[i] = Sudoku[i];
+
+        Sudoku[square] = option1;
+        SquareNullified(square);
+        RemovePossibilites(square, option1);
+        SolveFromPair();
+
+        IsSolved();
+        if (!sudokuSolved)
+        {
+            //reseting the current states
+            for (int i = 0; i < 81; i++)
+                for (int k = 0; k < 9; k++)
+                    sudokuPossibilities[i, k] = possibilitiesState[i, k];
+
+            for (int i = 0; i < 81; i++) Sudoku[i] = sudokuState[i];
+
+            Sudoku[square] = option2;
+            SquareNullified(square);
+            RemovePossibilites(square, option2);
+            SolveFromPair();
+
+            IsSolved();
+            if (!sudokuSolved)
+            {
+                //reseting the current states
+                for (int i = 0; i < 81; i++)
+                    for (int k = 0; k < 9; k++)
+                    sudokuPossibilities[i, k] = possibilitiesState[i, k];
+
+                for (int i = 0; i < 81; i++) Sudoku[i] = sudokuState[i];
+            }
+        }
+    }
+
+
+    public void ColumnPair(int column)
+    {
+        int square = column;
+        int pair = 0;
+
+        for (int i = 0; i < 9; i++)
+        {
+            if (Sudoku[square] == 0) pair++;
+            square += 9;
+        }
+        if (pair == 2)
+        {
+            square = column;
+            for (int i = 0; i < 9; i++)
+            {
+                if (Sudoku[square] == 0)
+                {
+                    int option1 = 0;
+                    int option2 = 0;
+                    for (int k = 0; k < 9; k++)
+                    {
+                        if (sudokuPossibilities[square, k] != null)
+                        {
+                            if (option1 == 0) option1 = (int)sudokuPossibilities[square, k];
+                            else if (option1 != 0) option2 = (int)sudokuPossibilities[square, k];
+                            else return;
+                        }
+                    }
+                    if (option1 != 0) GuessPair(square, option1, option2);
+                }
+                square += 9;
+            }
+        }
+    }
+
+    public void RowPair(int row)
+    {
+        int square = row * 9;
+        int pair = 0;
+
+        for (int i = 0; i < 9; i++)
+        {
+            if (Sudoku[square] == 0) pair++;
+            square++;
+        }
+        if (pair == 2)
+        {
+            square = row * 9;
+            for (int i = 0; i < 9; i++)
+            {
+                if (Sudoku[square] == 0)
+                {
+                    int option1 = 0;
+                    int option2 = 0;
+                    for (int k = 0; k < 9; k++)
+                    {
+                        if (sudokuPossibilities[square, k] != null)
+                        {
+                            if (option1 == 0) option1 = (int)sudokuPossibilities[square, k];
+                            else if (option1 != 0) option2 = (int)sudokuPossibilities[square, k];
+                            else return;
+                        }
+                    }
+                    if (option1 != 0) GuessPair(square, option1, option2);
+                }
+                square++;
+            }
+        }
+    }
 
     public void IsSolved()
     {
@@ -53,6 +244,127 @@ public class SudokuPuzzle
             if (Sudoku[i] == 0) return;
         }
         sudokuSolved = true;
+    }
+
+    public void PossibilityCheck()
+    {
+        for (int i = 0; i < 9; i++) RowCheck(i);
+        for (int i = 0; i < 9; i++) ColumnCheck(i);
+        for (int i = 0; i < 9; i++) QuadrantCheck(i);
+    }
+
+    public void QuadrantCheck(int quadrant)
+    {
+        var grid = new Grid();
+
+        for (int square = 0; square < 81; square++)
+        {
+            if (SquareInfo(square, "quadrant") == quadrant)
+            {
+                int[] squares = new int[9];
+                for (int m = 0; m < 9; m++)
+                {
+                    squares[m] = sudokuPossibilities[square, m] ?? 0;
+                }
+                grid.SquareCheck(squares);
+            }
+        }
+
+        int number = grid.SquareFound();
+        if (number != -1)
+        {
+            for (int square = 0; square < 81; square++)
+            {
+                if (SquareInfo(square, "quadrant") == quadrant)
+                {
+                    for (int m = 0; m < 9; m++)
+                    {
+                        if (sudokuPossibilities[square, m] == number)
+                        {
+                            Sudoku[square] = number;
+                            SquareNullified(square);
+                            RemovePossibilites(square, number);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void ColumnCheck(int column)
+    {
+        int square = column;
+        var grid = new Grid();
+
+        for (int k = 0; k < 9; k++)
+        {
+            int[] squares = new int[9];
+            for (int m = 0; m < 9; m++)
+            {
+                squares[m] = sudokuPossibilities[square, m] ?? 0;
+            }
+            grid.SquareCheck(squares);
+            square += 9;
+        }
+
+        int number = grid.SquareFound();
+        if (number != -1)
+        {
+            square = column;
+            for (int k = 0; k < 9; k++)
+            {
+                for (int m = 0; m < 9; m++)
+                {
+                    if (sudokuPossibilities[square, m] == number)
+                    {
+                        Sudoku[square] = number;
+                        SquareNullified(square);
+                        RemovePossibilites(square, number);
+                        return;
+                    }
+                }
+                square += 9;
+            }
+        }
+    }
+
+    public void RowCheck(int row)
+    {
+        int square = row * 9;
+        var grid = new Grid();
+
+        for (int k = 0; k < 9; k++)
+        {
+            int[] squares = new int[9];
+            for (int m = 0; m < 9; m++)
+            {
+                squares[m] = sudokuPossibilities[square, m] ?? 0;
+            }
+            grid.SquareCheck(squares);
+            square++;
+        }
+
+        int number = grid.SquareFound();
+        if (number != -1)
+        {
+            square = row * 9;
+            for (int k = 0; k < 9; k++)
+            {
+                for (int m = 0; m < 9; m++)
+                {
+                    if (sudokuPossibilities[square, m] == number)
+                    {
+                        Sudoku[square] = number;
+                        SquareNullified(square);
+                        RemovePossibilites(square, number);
+                        return;
+                    }
+                }
+                square++;
+            }
+        }
     }
 
     public void RemovePossibilitesCheck()
@@ -94,6 +406,7 @@ public class SudokuPuzzle
         {
             Sudoku[square] = number;
             SquareNullified(square); // Nullify possibilities after setting
+            RemovePossibilites(square, number);  //removes the possibilites from the new square
         }
     }
 
@@ -173,7 +486,7 @@ public class SudokuPuzzle
         else return -1;
     }
 
-    //readonly struct SquareInfo(int x, int row, int column, int quadrant);
+    
 
     public void PrintSudoku()
     {
@@ -183,5 +496,57 @@ public class SudokuPuzzle
             if ((i + 1) % 9 == 0)
                 Console.WriteLine();
         }
+    }
+
+    public struct Grid
+    {
+        int one;
+        int two;
+        int three;
+        int four;
+        int five;
+        int six;
+        int seven;
+        int eight;
+        int nine;
+
+        public Grid()
+        {
+        }
+
+        public void SquareCheck(int[] square)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                switch (square[i])
+                {
+                    case 1: one++; break;
+                    case 2: two++; break;
+                    case 3: three++; break;
+                    case 4: four++; break;
+                    case 5: five++; break;
+                    case 6: six++; break;
+                    case 7: seven++; break;
+                    case 8: eight++; break;
+                    case 9: nine++; break;
+                }
+            }
+        }
+
+        public int SquareFound()
+        {
+            if (one == 1) return 1;
+            if (two == 1) return 2;
+            if (three == 1) return 3;
+            if (four == 1) return 4;
+            if (five == 1) return 5;
+            if (six == 1) return 6;
+            if (seven == 1) return 7;
+            if (eight == 1) return 8;
+            if (nine == 1) return 9;
+
+            return -1;
+        }
+
     }
 }
